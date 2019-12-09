b41517fcccd8e67a833530b93478d3859bd23e30
imtaka <needer12345@gmail.com>
2019-12-09 19:01:24 +0900

    rspecを導入

--- config/application.rb
+++ config/application.rb
@@ -34,5 +34,17 @@ module Myapp
     config.time_zone = "Tokyo"
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.generators do |g|
+      g.test_framework :rspec,
+                       fixtures: true,
+                       view_specs: false,
+                       helper_specs: false,
+                       routing_specs: false,
+                       controller_specs: false,
+                       request_specs: true,
+                       model_spec: true
+      g.fixture_replacement :factory_bot, dir: "spec/factories"
+    end
   end
 end
