ba9275676140a97eff54d6317e18f0f5c3d2e7ac
imtaka <needer12345@gmail.com>
2019-12-10 17:55:12 +0900

    コメントの削除機能を追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -17,7 +17,14 @@ class CommentsController < ApplicationController
       redirect_to post_path(@post)
     end
   end
-  
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    if @comment.destroy
+      redirect_to @comment.post
+    end
+  end
+
   private
   def comment_params
     params.require(:comment).permit(:body, :user_id)
