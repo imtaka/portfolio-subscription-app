32ca681714b3b53f6c1fcbabf10c001bffc54a8c
imtaka <needer12345@gmail.com>
2019-12-14 22:02:54 +0900

    fontawesomeを導入

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -17,3 +17,4 @@ import "../stylesheets/application"
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+import "@fortawesome/fontawesome-free/js/all"
