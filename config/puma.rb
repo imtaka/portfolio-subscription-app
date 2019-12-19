5a0e35dc460244496eb2198017bd314bc92178ce
imtaka <needer12345@gmail.com>
2019-12-19 19:00:12 +0900

    dockerfileにnginxwo

--- config/puma.rb
+++ config/puma.rb
@@ -36,3 +36,6 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
+
+app_root = File.expand_path("../..", __FILE__)
+bind "unix://portfolio-subscription-app/tmp/sockets/puma.sock"
