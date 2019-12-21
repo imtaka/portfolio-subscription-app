358cee154cbaa72d693f3a28bc6013e8b23e68bf
imtaka <needer12345@gmail.com>
2019-12-21 14:12:57 +0900

    UserのModelにvalidation追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates :username, presence: true, length:{ maximum: 50 }
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable, :trackable, :omniauthable
