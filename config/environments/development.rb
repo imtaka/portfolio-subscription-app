47e693ec65d33d84b994f5a814536b6a635294b5
imtaka <needer12345@gmail.com>
2020-01-02 18:47:19 +0900

    mailer設定変更

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,7 +59,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'localhost:80' }
   config.action_mailer.delivery_method = :letter_opener_web
   config.after_initialize do
     Bullet.enable = true
@@ -68,16 +68,17 @@ Rails.application.configure do
     Bullet.console = true
     Bullet.rails_logger = true
   end
+
   config.hosts.clear
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :address => "smtp.gmail.com",
-    :port => 587,
-    :user_name => ENV['MAILADDRESS'],
-    :password => ENV['MAILPASSWORD'],
-    :authentication => :plain,
-    :enable_starttls_auto => true
-  }
+  address: 'smtp.gmail.com',
+  port: 587,
+  user_name: ENV['MAILADDRESS'],
+  password: ENV['MAILPASSWORD'],
+  authentication: 'plain',
+  enable_starttls_auto: true
+}
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -113,13 +113,20 @@ Rails.application.configure do
   
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    enable_starttls_auto: true,
-    port:                 587,
-    authentication:       'plain',
-    user_name: ENV['MAILADDRESS'],
-    password: ENV['MAILPASSWORD']
-  }
+  address: 'smtp.gmail.com',
+  port: 587,
+  user_name: ENV['MAILADDRESS'],
+  password: ENV['MAILPASSWORD'],
+  authentication: 'plain',
+  enable_starttls_auto: true
+}
+  # config.action_mailer.smtp_settings = {
+  #   address:              'smtp.gmail.com',
+  #   enable_starttls_auto: true,
+  #   port:                 587,
+  #   authentication:       'plain',
+  #   user_name: ENV['MAILADDRESS'],
+  #   password: ENV['MAILPASSWORD']
+  # }
 end
